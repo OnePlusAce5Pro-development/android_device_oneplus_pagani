8db1d7b7c62a77341bd0f651f1163acaa6d1c71e
awa <2172245494@qq.com>
2026-08-24 02:15:44 +0800

    hummer: Update from PKR110_11.C.29

--- custom_hummer.mk
+++ custom_hummer.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := PKR110
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi-user 16 BP2A.250605.015 1783396348784 release-keys" \
-    BuildFingerprint=OnePlus/PKR110/OP60EBL1:16/BP2A.250605.015/1783396348784:user/release-keys \
+    BuildDesc="qssi-user 16 BP2A.250605.015 1786442564953 release-keys" \
+    BuildFingerprint=OnePlus/PKR110/OP60EBL1:16/BP2A.250605.015/1786442564953:user/release-keys \
     DeviceName=OP60EBL1 \
     DeviceProduct=PKR110 \
     SystemDevice=OP60EBL1 \
